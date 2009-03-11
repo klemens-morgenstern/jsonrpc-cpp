4eeee8e5d42298968be5f521eb2cea97f489c0cf
s-vincent <s-vincent@d4c62b91-c5ef-480b-b16d-56bd71309914>
2009-03-11 08:48:07 +0000

    Tag jsonrpc-cpp-0.1.3.

--- test/udp-server.cpp
+++ test/udp-server.cpp
@@ -9,24 +9,52 @@
 
 #include <jsonrpc/jsonrpc.h>
 
-#include "test.h"
+class Plop
+{
+  public:
+    bool Print(const Json::Value& root, Json::Value& response)
+    {
+      std::cout << "Plop" << std::endl;
+      response["jsonrpc"] = "2.0";
+      response["id"] = root["id"];
+      response["result"] = "success";
+      return true;
+    }
 
-/**
- * \brief Entry point of the program.
- * \param argc number of argument
- * \param array of arguments
- * \return EXIT_SUCCESS or EXIT_FAILURE
- */
+    Json::Value GetDescription()
+    {
+      Json::FastWriter writer;
+      Json::Value root;
+      Json::Value parameters;
+      Json::Value param1;
+      
+      root["description"] = "Print Plop";
+
+      /* type of parameter named arg1 */
+      param1["type"] = "integer";
+      param1["description"] = "argument 1";
+
+      /* push it into the parameters list */
+      parameters["arg1"] = param1;
+      root["parameters"] = parameters;
+
+      /* no value returned */
+      root["returns"] = Json::Value::null;
+
+      return root;
+    }
+};
+
 int main(int argc, char** argv)
 {
-  Test a;
+  Plop a;
   Json::Rpc::UdpServer server(std::string("127.0.0.1"), 8086);
 
   /* to avoid compilation warnings */
   argc = argc;
   argv = argv;
 
-  server.AddMethod(new Json::Rpc::RpcMethod<Test>(a, &Test::Print, std::string("system.print"), a.GetDescription()));
+  server.AddMethod(new Json::Rpc::RpcMethod<Plop>(a, &Plop::Print, std::string("system.print"), a.GetDescription()));
 
 /*  server.SetEncapsulatedFormat(Json::Rpc::NETSTRING); */
 
